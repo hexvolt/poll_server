e03b79df33a15c9e629a29f5bacf7e546e483641
hexvolt <hexvolt@gmail.com>
2016-04-07 23:10:12 +0300

    ticket:1 polls app added, initial migration

--- poll_server/settings.py
+++ poll_server/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
