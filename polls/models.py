fb07730208361241c24830a45a592e217cd0e8cd
hexvolt <hexvolt@gmail.com>
2016-04-13 21:48:50 +0300

    ticket:3 messages built up

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,12 @@ class Question(models.Model):
     class Meta:
         ordering = ['-pub_date']
 
+    @property
+    def serialized_data(self):
+        from polls.serializers import QuestionSerializer
+
+        return QuestionSerializer(self).data
+
     @python_2_unicode_compatible
     def __str__(self):
         return self.question_text
@@ -26,6 +32,12 @@ class Choice(models.Model):
     def vote(self):
         self.votes += 1
         self.save()
+
+    @property
+    def serialized_data(self):
+        from polls.serializers import ChoiceSerializer
+
+        return ChoiceSerializer(self).data
 
     @python_2_unicode_compatible
     def __str__(self):
--- publisher/signals.py
+++ publisher/signals.py
@@ -1,3 +1,5 @@
+import json
+
 from django.conf import settings
 
 
@@ -5,20 +7,28 @@ def on_model_save(sender, rabbit_client, **kwargs):
     instance = kwargs.get('instance')
     is_created = kwargs.get('created')
 
-    message = "{} model {}".format(
-        str(sender), 'created' if is_created else 'modified')
+    message = {
+        'model': sender.__name__,
+        'action': 'created' if is_created else 'updated',
+        'instance': instance.serialized_data,
+    }
 
     rabbit_client.send_message(
-        exchange_name=settings.RABBITMQ_APP_EXCHANGE, message=message
+        exchange_name=settings.RABBITMQ_APP_EXCHANGE,
+        message=json.dumps(message)
     )
 
 
 def on_model_delete(sender, rabbit_client, **kwargs):
-    print(sender)
     instance = kwargs.get('instance')
 
-    message = "{} model deleted".format(str(sender))
+    message = {
+        'model': sender.__name__,
+        'action': 'deleted',
+        'instance': instance.serialized_data,
+    }
 
     rabbit_client.send_message(
-        exchange_name=settings.RABBITMQ_APP_EXCHANGE, message=message
+        exchange_name=settings.RABBITMQ_APP_EXCHANGE,
+        message=json.dumps(message)
     )
